c3275c695ed84548b9177275fee3ae73d48f4a21
Gurgent Singh <singhgurgent1@gmail.com>
2019-01-23 11:22:01 -0500

    hello

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            for (int i = 14; i > 10;)
+            {
+                i++;
+                sum += i;
+            }
+            Console.WriteLine("the sum is : {0}"+sum);
+            Console.ReadLine();
         }
     }
-}
+    }
+
